b1c19a349f2146b83e4eca3d87102e1e64e68758
HaoNguyen-Ron <hao100319@gmail.com>
2023-09-24 15:56:23 +0700

    fixing some bug

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const { default: mongoose } = require('mongoose');
-const routes = require('./router')
+const cors = require('cors');
 const passport = require('passport');
+const routes = require('./router')
+const { default: mongoose } = require('mongoose');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const { DB_NAME } = require('./constants/db');
+
 require('dotenv').config();
 
 const {
@@ -26,11 +29,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add CORS here
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+
 // mongodb connection
 
 // // mongoose.connect('mongodb://localhost:27017/node-32-database');
 // mongoose.connect('mongodb://127.0.0.1:27017/node-32-database');
-mongoose.connect('mongodb+srv://saltymeatball:0zs2KQoCdUe8pAYY@cluster0.f1rylpl.mongodb.net/node-32-database');
+mongoose.connect(`mongodb+srv://${DB_NAME}`);
 
 passport.use(passportVerifyToken);
 passport.use(passportVerifyAccount);
